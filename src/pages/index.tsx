3bc88daea4f62a624aa2dfee62f00d1d21b978d9
miqbals17 <yakuzuyoa@gmail.com>
2024-10-17 12:05:53 +0700

    fix: print cookies for debugging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
 import Home from "@/components/pages/Home/Home";
 import Page from "@/components/ui/Page";
-import sendFirebase from "@/utils/firebase";
 import Head from "next/head";
-import { useEffect } from "react";
 
 export default function HomePage() {
-  useEffect(() => {
-    sendFirebase({
-      id_list_product: 829001,
-      list_product_name: "cek-kelaikan-kendaraan",
-    });
-  }, []);
-
   return (
     <Page homePage pageTitle="Cek Kelaikan Kendaraan">
       <Head>
